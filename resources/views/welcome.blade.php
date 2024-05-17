061ccc907a7839240dfc97d561d1973ba18138cc
ammangdeveloper <ammangdevelope@gmail.com>
2024-05-17 15:16:10 +0800

    slideshow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,19 +18,19 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="{{ asset('assets/images/slideshow/slide-show-1.jpg') }}" class="d-block w-100 img-fluid"
+                        <img src="{{ asset('assets/images/slideshow/slide-show-1.jpg') }}" class="d-block w-100 slide-cover"
                             alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="{{ asset('assets/images/slideshow/slide-show-2.jpg') }}" class="d-block w-100"
+                        <img src="{{ asset('assets/images/slideshow/slide-show-2.jpg') }}" class="d-block w-100 slide-cover"
                             alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="{{ asset('assets/images/slideshow/slide-show-4.jpg') }}" class="d-block w-100"
+                        <img src="{{ asset('assets/images/slideshow/slide-show-4.jpg') }}" class="d-block w-100 slide-cover"
                             alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="{{ asset('assets/images/slideshow/slide-show-5.jpg') }}" class="d-block w-100"
+                        <img src="{{ asset('assets/images/slideshow/slide-show-5.jpg') }}" class="d-block w-100 slide-cover"
                             alt="...">
                     </div>
                 </div>
